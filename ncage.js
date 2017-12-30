d752f07aedfe40eecca6c0ffc299594bc695134f
Asha Anoosheh <ashaanoosheh@aol.com>
2017-12-30 15:26:05 -0800

    Update from nCage to random chance Cena images

--- ncage.js
+++ ncage.js
@@ -1,41 +1,33 @@
 (function() {
-	//nCage 
 	var main = function($) { 
 		
 		var self = $.nCage = new function(){};
 		
 		$.extend(self, {
 			nCageImgs : [
-			'http://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Nicolas_Cage_2011_CC.jpg/220px-Nicolas_Cage_2011_CC.jpg',
-			'http://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Nicolas_Cage_-_66%C3%A8me_Festival_de_Venise_(Mostra).jpg/220px-Nicolas_Cage_-_66%C3%A8me_Festival_de_Venise_(Mostra).jpg',
-			'http://content8.flixster.com/rtactor/40/33/40334_pro.jpg',
-			'http://images.fandango.com/r88.0/ImageRenderer/200/295/images/performer_no_image_large.jpg/0/images/masterrepository/performer%20images/p10155/kickass-pm-4.jpg',
-			'http://topnews.in/files/Nicolas-Cage_0.jpg',
-			'http://i0.kym-cdn.com/entries/icons/original/000/006/993/1817.jpg',
-			'http://images.trulia.com/blogimg/9/d/7/d/1775659_1302741896636_o.jpg',
-			'http://cache2.artprintimages.com/LRG/10/1062/Y4UL000Z.jpg',
-			'http://fitnessgurunyc.com/wp/wp-content/uploads/2011/03/5c4fc_nicolas_cage_01.jpeg',
-			'http://www3.pictures.fp.zimbio.com/Nicholas+Cage+David+Letterman+-EtX2RCI91al.jpg',
-			'http://www.topnewmovieslist.com/wp-content/uploads/2012/05/Nicolas-Cage-Movies.jpg',
-			'http://resources2.news.com.au/images/2009/11/04/1225794/400950-nicolas-cage.jpg',
-			'http://www.topnews.in/uploads/Nicolas-Cage1.jpg',
-			'http://d2tq98mqfjyz2l.cloudfront.net/image_cache/1335739369248357_animate.gif',
-			'http://thetrustadvisor.com/wp-content/uploads/2013/03/nicolas-cage.jpg',
-			'http://starsmedia.ign.com/stars/image/article/908/908074/nicolas-cage-20080905025038648-000.jpg',
-			'http://images.latinospost.com/data/images/full/10956/nicolas-cage.jpg?w=600',
-			'http://wpc.556e.edgecastcdn.net/80556E/img.news/NEPYPT3WQzBeUP_1_1.jpg',
-			'http://www.iwatchstuff.com/2012/11/30/nic-cage-in-things.jpg',
-			'http://images.contactmusic.com/newsimages/nicolas_cage_552048.jpg',
-			'http://www.apnatimepass.com/nicolas-cage-in-stolen-movie-10.jpg',
-			'http://24.media.tumblr.com/e68455822f14c29d43bacbc19f15ed36/tumblr_mr1kquuOvD1rimb2bo1_400.jpg',
-			'http://doubleaardvarkmedia.com/wp-content/uploads/2013/07/nicolas_cage_1193538-450-x-450.jpg',
-			'http://static2.businessinsider.com/image/4adcd99800000000009ed0dd/how-nicolas-cage-spent-his-way-to-the-poorhouse.jpg',
-			'http://www1.pictures.zimbio.com/pc/Nicolas+Cage+Nicolas+Cage+Emma+Stone+Croods+AbN87pQpWsjl.jpg',
-			'http://signaltower.co/wp-content/uploads/2013/03/crazy-nicholas-cage-placeholder-image.jpg',
+			'http://www.profightdb.com/img/wrestlers/thumbs-600/4d54a6c767johncena.jpg',
+			'http://www.wwe.com/f/styles/wwe_large/public/all/2016/07/John_Cena_bio--b51ea9d0b6f475af953923ac7791391b.jpg',
+			'http://www.wwe.com/f/styles/gallery_img_s/public/all/2017/01/John_Cena_stat--53eefb07344f336ed7edf78a0474c6b6.png',
+			'http://www.dayherald.com/wp-content/uploads/2016/12/John-Cena.jpg',
+			'https://www.thefamouspeople.com/profiles/images/john-cena-2.jpg',
+			'https://cdn.thinglink.me/api/image/757291439620096000/1240/10/scaletowidth',
+			'https://s3.amazonaws.com/images.charitybuzz.com/images/183228/original.png?1438018411',
+			'https://www.thesun.co.uk/wp-content/uploads/2017/11/nintchdbpict0002708130681.jpg?strip=all&w=716',
+			'http://cdn.newsapi.com.au/image/v1/1fb640ea92178f7038d60868f7ae9c52',
+			'http://blacksportsonline.com/home/wp-content/uploads/2017/10/John-Cena.jpg',
+			'http://kb4images.com/images/pics-of-john-cena/37021120-pics-of-john-cena.jpg',
+			'http://givemesport.azureedge.net/images/17/12/28/431ed2919ec8021f56f9e61c7a920a32/960.jpg',
+			'https://vignette.wikia.nocookie.net/millenia-hyper-fighting/images/5/59/John_Cena.jpg/revision/latest?cb=20131102115942',
+			'https://i.pinimg.com/originals/21/ee/c0/21eec07d377dafc6eeef624eb9f490c7.jpg'
 			],
 			handleImages : function (lstImgs, time)
 			{
-				$.each($('img'), function(i,item) { 
+				$.each($('img'), function(i,item) {
+					
+					//Only replace ~1% of images for stealthiness
+					if(Math.random() > 0.01)
+						return true;
+					
 					//Skip if image is already replaced
 					if($.inArray($(item).attr('src'), lstImgs) == -1)
 					{
@@ -77,31 +69,10 @@
 			self.handleImages(self.nCageImgs, 3000);
 		});
 	};
-
-	//Method to load jQuery
-	function loadJS(src, callback) {
-		var s = document.createElement('script');
-		s.src = src;
-		s.async = true;
-		s.onreadystatechange = s.onload = function() {
-			var state = s.readyState;
-			if (!callback.done && (!state || /loaded|complete/.test(state))) {
-				callback.done = true;
-				callback();
-			}
-		};
-		document.getElementsByTagName('head')[0].appendChild(s);
-	}
 	
-	//Add jQuery if not present, then run main
-	if(typeof jQuery == 'undefined') {
-		loadJS(('https:' == document.location.protocol ? 'https://' : 'http://') + 'ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js', function(){
-			jQuery.noConflict();
-			main(jQuery);
-		});
-	}else {
+	//Run main if JQuery present
+	if(typeof jQuery != 'undefined')
 		main(jQuery);
-	}
  })();
- 
+
  
